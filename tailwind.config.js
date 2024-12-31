599411dc2631da3bea6de3bbbc187b7636c2c821
virtuousfinancier <virtuousfinancier@gmail.com>
2024-12-31 14:37:52 +0900

    update the Tailwind config to include our fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'barlow': ['Barlow', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
