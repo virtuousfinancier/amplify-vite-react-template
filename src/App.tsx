8b3f325ddae5d9805929fe1f0f131543a9f895f9
virtuousfinancier <virtuousfinancier@gmail.com>
2024-12-31 11:50:03 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import vfiPNG from './assets/virtuousfinance-icon-500x500px.png'
+import vfiSVG from '/virtuousfinance-icon-500x500px.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import VolatilitySurface from './components/VolatilitySurface'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container mx-auto p-4">
+      <VolatilitySurface />
+      <img src={vfiPNG} className="logo vfi" alt="vfi logo" />
+      <img src={vfiSVG} className="logo vfi" alt="vfi logo" />
+    </div>
   )
 }
 
